e993e43495d146779cc6676136c78ec791d8228e
Jonathan Tuzman <tuzmusic@gmail.com>
2019-01-04 10:43:47 -0500

    redirect for invalid artist

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,7 +1,12 @@
 class SongsController < ApplicationController
   def index
     if params[:artist_id]
-      @songs = Artist.find(params[:artist_id]).songs
+      artist = Artist.find_by_id(params[:artist_id])
+      if artist
+        @songs = artist.songs
+      else
+        redirect_to artists_path, flash: { alert: "Artist not found" }
+      end
     else
       @songs = Song.all
     end
