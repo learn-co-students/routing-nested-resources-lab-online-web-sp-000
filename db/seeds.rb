7212f8c0b08fa3b4061f21916bab9c3eac9ac7f3
Richard Huelves <rhuelves@gmail.com>
2019-12-18 10:56:53 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,10 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 #
+# puts 'deleting songs'
+# Song.destroy_all
+
+puts 'creating songs'
 songs = [{artist_name: "The Killers", title: "Mr. Brightside"},
          {artist_name: "The Killers", title: "Jenny Was a Friend of Mine"},
          {artist_name: "Tommy Tutone", title: "Jenny"},
