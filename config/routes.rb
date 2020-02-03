6b9c41fe55daa2b5050ea4afed5f273e6bd5f98a
Rehman Ali <rehmanali001@gmail.com>
2020-02-03 14:23:14 -0600

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :artists
-  resources :songs
+  resources :artists, only: [:index, :show, :new, :create, :edit, :update] do
+    resources :songs, only: [:show, :index]
+  end 
+  resources :songs, only: [:index, :show, :new, :create, :edit, :update]
 end
