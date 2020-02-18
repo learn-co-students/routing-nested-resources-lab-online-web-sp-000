baf088486ea5470944b42cf3e8c71d68414d77c6
rohanphillips <rohan@solutions-insurance.com>
2020-02-18 07:52:41 -0600

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
+# Rails.application.routes.draw do
+#   resources :artists
+#   resources :songs
+# end
+
 Rails.application.routes.draw do
-  resources :artists
-  resources :songs
+ 
+  resources :artists, only: [:show] do
+    # nested resource for posts
+    resources :songs, only: [:show, :index]
+  end
+  
+  resources :artists, only: [:index]
+  resources :songs, only: [:index, :show, :new, :create, :edit, :update]
+ 
+  root 'songs#index'
 end
