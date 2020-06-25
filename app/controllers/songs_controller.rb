921ad88568e7edd9dde308c184e925b37a888754
mlgvla <mlgerardvla@gmail.com>
2020-06-25 18:24:56 -0400

    Testing

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,9 +1,27 @@
 class SongsController < ApplicationController
   def index
+    if params[:artist_id]
+      @artist = Artist.find_by(id: params[:artist_id])
+      if @artist.nil?
+        flash[:alert] = "Artist not found"
+        redirect_to artists_path   
+      else
+        @songs = @artist.songs
+      end
+    end
+
     @songs = Song.all
   end
 
   def show
+    if params[:artist_id]
+      @artist = Artist.find_by(id: params[:artist_id])
+      @song = @artist.songs.find_by(id: params[:id])
+      if @song.nil?
+        flash[:alert] = "Song not found"
+        redirect_to artist_songs_path(@artist)
+      end
+    end
     @song = Song.find(params[:id])
   end
 
