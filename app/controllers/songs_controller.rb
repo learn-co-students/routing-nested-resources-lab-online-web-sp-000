0ee4fda9f6b7bcfb8076a554fbe67a1ac0fad256
mdacht777 <mdacht777@gmail.com>
2020-10-08 06:55:52 -0600

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,10 +1,32 @@
 class SongsController < ApplicationController
   def index
-    @songs = Song.all
+    if params[:artist_id]
+    # binding.pry
+      @artist = Artist.find_by(id: params[:artist_id])
+      if @artist.nil?
+        flash[:error] = "Artist not found"
+        redirect_to artists_path
+      else
+        @songs = @artist.songs
+      end
+    else
+      @songs = Song.all
+    end
   end
 
   def show
-    @song = Song.find(params[:id])
+    if params[:artist_id]
+    # binding.pry
+      @artist = Artist.find_by(id: params[:artist_id])
+      if @artist.songs.find_by(id: params[:id]).nil?
+        flash[:alert] = "Song not found"
+        redirect_to artist_songs_path(@artist)
+      else
+        @song = @artist.songs.find_by(id: params[:id])
+      end
+    else
+      @song = Song.find_by(id: params[:id])
+    end
   end
 
   def new
@@ -43,6 +65,26 @@ class SongsController < ApplicationController
     flash[:notice] = "Song deleted."
     redirect_to songs_path
   end
+
+#   def artists_index
+#     @artist = Artist.find(params[:id])
+# binding.pry
+#     if @artist
+#       @songs = @artist.songs
+#       render template: 'songs/index'
+#     else
+#       redirect_to artists_path
+#     end
+#   end
+ 
+#   def artist
+#     @artist = Artist.find(params[:id])
+ 
+#     # Note that because ids are unique by table we can go directly to
+#     # song.find — no need for @artist.songs.find...
+#     @song = Song.find(params[:song_id])
+#     render template: 'songs/show'
+#   end
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  resources :artists
+  # get 'artists/:id/songs', to: 'songs#artists_index'
+  # get 'artists/:id/songs/:song_id', to: 'songs#artist'
+  # resources :artists
+  resources :artists do
+    resources :songs, only: [:index, :show]
+  end
   resources :songs
+
 end
