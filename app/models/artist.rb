34a4aa1a592959287885e5e65590fb08cbcace6b
yarvinh <yarvinhhernandez@gmail.com>
2020-10-12 00:20:03 -0400

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,11 @@
 class Artist < ActiveRecord::Base
   has_many :songs
+  
+  def self.find_by_id(id)
+    if id
+      Artist.all.select{|artist| artist.id.to_i == id.to_i}[0]
+    else
+      false
+    end
+  end
 end
