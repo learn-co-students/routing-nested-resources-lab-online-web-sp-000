a5e4fe5cdf744b24d4256ef9ef79fdf28d73987f
dmepilot <dmepilot@yahoo.com>
2021-02-03 17:37:21 -0700

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,5 +1,13 @@
 module ArtistsHelper
+  # def display_artist(song)
+  #   song.artist.nil? ? link_to("Add Artist", edit_song_path(song)) : link_to(song.artist_name, artist_path(song.artist))
+  # end
+
   def display_artist(song)
-    song.artist.nil? ? link_to("Add Artist", edit_song_path(song)) : link_to(song.artist_name, artist_path(song.artist))
-  end
+    if song.artist
+        link_to song.artist_name, artist_path(song.artist)
+    else
+        link_to 'Add Artist', edit_song_path(song)
+    end
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :artists, only: [:show] do
+    # nested resource for posts
+    resources :songs, only: [:show, :index]
+  end
+  
   resources :artists
   resources :songs
 end
