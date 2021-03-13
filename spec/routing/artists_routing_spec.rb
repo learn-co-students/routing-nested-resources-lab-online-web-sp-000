95f9c7cf1394b449582296eec758476e6739326d
Alan Tang <giabao_tang@yahoo.com>
2021-03-12 16:28:11 -0800

    Done.

--- spec/routing/artists_routing_spec.rb
+++ spec/routing/artists_routing_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe "artists routing", type: :routing do
   it "routes to a song as a nested resource of an artist" do
     expect(get: artist_song_path(1, 2)).to route_to("songs#show", artist_id: "1", id: "2")
   end
-
+ 
   it 'only routes to index and show for songs' do
     base_route = "/artists/1/songs"
     expect(get: base_route + "/new").not_to route_to("songs#new")
