5820b24f6e642d2a6aafcdb7adb555a4f2b70010
Christerle Nounoute <christerlesn@gmail.com>
2021-05-08 03:35:11 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :artists
-  resources :songs
+  resources :artists do
+    resources :songs, only: [:show, :index]
+  end
+resources :songs, :artists, only: [:show, :index, :new, :create, :edit, :update]
 end
